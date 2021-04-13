340c3f2ee4b36ec7c3f86b0295c34349f104a41e
Parokonnyy <parokonnyy.work@gmail.com>
2021-04-13 19:17:21 +0300

    Initial commit

---
+++ SexySQL/Database.cs
@@ -0,0 +1,7 @@
+namespace SexySQL
+{
+    public class Database
+    {
+        
+    }
+}
---
+++ SexySQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SexySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
