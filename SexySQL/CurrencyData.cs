5d888cc46a9509389d59290a695560350ec1064c
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2021-04-13 19:19:49 +0300

    Main class added

---
+++ SexySQL/CurrencyData.cs
@@ -0,0 +1,11 @@
+namespace SexySQL
+{
+    public class CurrencyData
+    {
+        public DateTime Date { get; set; }
+        public double Dollar { get; set; }
+
+        public double Euro { get; set; }
+        public double  Jena { get; set; }
+    }
+}
